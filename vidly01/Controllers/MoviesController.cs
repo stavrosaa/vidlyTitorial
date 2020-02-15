bbb5383638b775abb1adb9d79a8f59554a366637
staurossigelakis <staurossigelakis01@gmail.com>
2020-02-15 14:50:21 +0200

    createRandonController

---
+++ vidly01/Controllers/MoviesController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using vidly01.Models;
+
+namespace vidly01.Controllers
+{
+    public class MoviesController : Controller
+    {
+
+        //Get:Movies/Random
+       public ActionResult Random()
+        {
+
+            var movie = new Movie() { Name = "H Megalh Apofraji" };
+            return View(movie);
+        }
+
+
+
+        // GET: Movies
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
