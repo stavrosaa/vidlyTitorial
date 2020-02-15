a4093f07c8dd33a08b23d65735deb0eb3663fea3
staurossigelakis <staurossigelakis01@gmail.com>
2020-02-15 15:23:07 +0200

    ChangeDarckTemplate

--- vidly01/App_Start/BundleConfig.cs
+++ vidly01/App_Start/BundleConfig.cs
@@ -23,7 +23,7 @@ namespace vidly01
                       "~/Scripts/bootstrap.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/bootstrap-darck.css",
                       "~/Content/site.css"));
         }
     }
